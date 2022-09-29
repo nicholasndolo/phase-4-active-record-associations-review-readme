64dde51602a8240aa68147fb2381c9b60d44a941
Nicholas <nicholas.nmalinda@gmail.com>
2022-09-29 20:47:25 +0300

    Active Record Associations Review

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,4 @@
 class PostTag < ApplicationRecord
+    belongs_to :author
+    belongs_to :tag
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+    has_many :tags
+    has_many :posts, through: :posts_tags
 end
